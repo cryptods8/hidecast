37cb53de6964dc6d9134d89d6dcd127069e8dd5f
dusan <cryptods8>
2024-08-16 14:13:32 +0200

    fixes

--- ui/sign-in.tsx
+++ ui/sign-in.tsx
@@ -11,73 +11,87 @@ import {
 import {
   ArrowLeftStartOnRectangleIcon,
   ChevronDownIcon,
+  ArrowPathIcon,
 } from "@heroicons/react/16/solid";
 import { Session } from "next-auth";
 import { getCsrfToken, signIn, signOut, useSession } from "next-auth/react";
-import Link from "next/link";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 function ProfileButton({
   session,
   onSignOut,
+  isLoading,
 }: {
-  session: Session;
+  session?: Session;
   onSignOut: () => void;
+  isLoading?: boolean;
 }) {
   return (
     <div className="inline-flex flex-row text-white">
-      <Link
-        href={`/profiles/fc/${session.user?.name}`}
-        className="bg-primary-400 w-full px-5 py-3 text-lg font-bold rounded-l-lg hover:bg-primary-400/80 active:bg-primary-400/70 inline-flex items-center gap-2 transition duration-150 ease-in-out"
+      <div
+        className={`bg-primary-400 w-full px-4 py-2 text-lg font-bold bg-action-primary inline-flex items-center gap-2 transition duration-150 ease-in-out ${
+          isLoading ? "rounded-lg" : "rounded-l-lg"
+        }`}
       >
-        {session?.user?.image ? (
+        {isLoading ? (
+          <div className="size-8 animate-spin flex items-center justify-center">
+            <ArrowPathIcon className="size-4" />
+          </div>
+        ) : session?.user?.image ? (
           <img src={session?.user.image} className="w-8 h-8 rounded-full" />
         ) : null}
-      </Link>
-      <Menu>
-        <MenuButton className="inline-flex items-center rounded-r-lg bg-primary-400 py-3 px-3 data-[hover]:bg-primary-400/80 data-[open]:bg-primary-400/70 border-l border-primary-500 transition duration-150 ease-in-out">
-          <ChevronDownIcon className="size-6 fill-white/80" />
-        </MenuButton>
-        <Transition
-          enter="transition ease-out duration-75"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <MenuItems
-            anchor="bottom end"
-            className="w-52 origin-top-right mt-1 rounded-lg text-white bg-primary-500 font-bold [--anchor-gap:var(--spacing-1)] focus:outline-none shadow-lg"
+      </div>
+      {!isLoading && (
+        <Menu>
+          <MenuButton className="inline-flex items-center rounded-r-lg bg-action-primary py-3 px-3 data-[hover]:bg-action-primary/80 data-[open]:bg-action-primary/70 border-l border-black/10 transition duration-150 ease-in-out">
+            <ChevronDownIcon className="size-6 fill-white/80" />
+          </MenuButton>
+          <Transition
+            enter="transition ease-out duration-75"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
           >
-            <MenuItem>
-              <button
-                className="group flex w-full items-center gap-2 rounded-lg py-3 px-5 bg-primary-400 data-[focus]:bg-primary-400/80 transition duration-150 ease-in-out"
-                onClick={onSignOut}
-              >
-                <ArrowLeftStartOnRectangleIcon className="size-5 fill-white/80" />
-                Sign out
-              </button>
-            </MenuItem>
-          </MenuItems>
-        </Transition>
-      </Menu>
+            <MenuItems
+              anchor="bottom end"
+              className="w-52 origin-top-right mt-1 rounded-lg text-white bg-action-primary font-bold [--anchor-gap:var(--spacing-1)] focus:outline-none shadow-lg"
+            >
+              <MenuItem>
+                <button
+                  className="group flex w-full items-center gap-2 rounded-lg py-3 px-5 bg-action-primary data-[focus]:bg-action-primary/80 transition duration-150 ease-in-out"
+                  onClick={onSignOut}
+                >
+                  <ArrowLeftStartOnRectangleIcon className="size-5 fill-white/80" />
+                  Sign out
+                </button>
+              </MenuItem>
+            </MenuItems>
+          </Transition>
+        </Menu>
+      )}
     </div>
   );
 }
 
 export function SignIn() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
   const [error, setError] = useState(false);
+  const [nonce, setNonce] = useState<string | null>(null);
 
-  const getNonce = useCallback(async () => {
-    const nonce = await getCsrfToken();
-    if (!nonce) {
-      throw new Error("Unable to generate nonce");
-    }
-    return nonce;
-  }, []);
+  useEffect(() => {
+    // somehow this is necessary to get the "good" nonce
+    new Promise((res) => setTimeout(res, 150))
+      .then(() => getCsrfToken())
+      .then((nonce) => {
+        if (!nonce) {
+          throw new Error("Unable to generate nonce");
+        }
+        setNonce(nonce);
+      });
+  }, [session]);
 
   const handleSuccess = useCallback((res: StatusAPIResponse) => {
     signIn("credentials", {
@@ -85,23 +99,42 @@ export function SignIn() {
       signature: res.signature,
       name: res.username,
       pfp: res.pfpUrl,
-      redirect: true,
-    });
+      redirect: false,
+    })
+      .then((x) => {
+        if (x?.error || !x?.ok) {
+          throw new Error("Error signing in: " + (x?.error || "unknown"));
+        }
+        window.location.reload();
+      })
+      .catch((e) => {
+        console.error("Error signing in", e);
+        signOut();
+      });
   }, []);
 
   const handleSignOut = useCallback(() => {
     signOut();
   }, []);
 
+  const isLoading = status === "loading" || !nonce;
+
   return (
     <div className="flex flex-row gap-2">
-      {session ? (
-        <ProfileButton session={session} onSignOut={handleSignOut} />
+      {isLoading || session ? (
+        <ProfileButton
+          session={session ?? undefined}
+          onSignOut={handleSignOut}
+          isLoading={isLoading}
+        />
       ) : (
         <SignInButton
-          nonce={getNonce}
+          key={nonce}
+          nonce={nonce!}
           onSuccess={handleSuccess}
-          onError={() => setError(true)}
+          onError={() => {
+            setError(true);
+          }}
           onSignOut={() => signOut()}
         />
       )}
